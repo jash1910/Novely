20b42f6962fb551159533df1d26c36964bc8439f
Jashvitha Lakshmi <omkaramlakshmi@Jashvithas-MacBook-Pro.local>
2025-11-27 15:30:41 +0530

    fixing error

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  
 
 connectDB();
 
